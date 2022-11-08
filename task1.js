0148a3d78dec6e33ed8c46ebd061f25763aac2c6
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2022-11-08 12:21:30 +0000

    task1

--- task1.js
+++ task1.js
@@ -1,3 +1,7 @@
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+const capitalisedCats = cats.map(function(cat){
+
+    return cat.toUpperCase();
+})
+console.log(capitalisedCats)
