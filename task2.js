cb7bc765851def17ac6fe49a060ba84bcc36c775
[Creg Grady] <112422397+CregCodes@users.noreply.github.com>
2022-11-08 12:31:04 +0000

    Finished task 2

--- task2.js
+++ task2.js
@@ -8,4 +8,8 @@ let animals = [
     "basalisk",
   ];
   
-let bListAnimals = // Your 'filter' code here.
+let bListAnimals = animals.filter(function(animal){
+  return animal[0] === "b";
+});
+
+console.log(bListAnimals());
